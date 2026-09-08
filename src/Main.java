efbd2d6ed2f52756b39c85899a02db5c414d7f80
FellypeGC <gabrielfellypesteve@gmail.com>
2026-09-07 21:05:30 -0300

    development patterns and concepts

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import java.util.*;
+
 public class Main{
 
+    private final static String WELCOME_MESSAGE = "Olá, informe o seu nome";
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        String msg = "Programando em";
-        System.out.println(msg + args[0]);
-        System.out.println("Precisamos de uma " + args[1]);
+        /* My comment here */
+        var scanner = new Scanner(System.in);
+        System.out.println(WELCOME_MESSAGE);
+        String name = scanner.next();
+
+        System.out.println("Olá, informe a sua idade");
+        int age = scanner.nextInt();
+
+        System.out.printf("Olá, %s! Sua idade é %s", name, age);
     }
 
 }
