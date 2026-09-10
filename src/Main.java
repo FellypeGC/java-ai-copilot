22e7721b84be75a1a34d9712cd15294a90fda0e6
FellypeGC <gabrielfellypesteve@gmail.com>
2026-09-10 00:52:24 -0300

    some (2) primitive types

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,9 @@
-import java.util.*;
-
 public class Main{
 
-    private final static String WELCOME_MESSAGE = "Olá, informe o seu nome";
-
     public static void main(String[] args) {
-        /* My comment here */
-        var scanner = new Scanner(System.in);
-        System.out.println(WELCOME_MESSAGE);
-        String name = scanner.next();
-
-        System.out.println("Olá, informe a sua idade");
-        int age = scanner.nextInt();
-
-        System.out.printf("Olá, %s! Sua idade é %s", name, age);
+        var number = 1.0d;
+        boolean character = true;
+        System.out.println("Those variables above are called primitive types");
     }
 
 }
